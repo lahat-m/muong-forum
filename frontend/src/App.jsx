685f27a55d9a4a5ec16c08e42f3fbf76da65ec25
lahat-m <lahatm.apach@gmail.com>
2025-05-10 07:50:27 +0300

    Re-organized routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AuthPage from './pages/AuthPage'
 import Dashboard from './pages/Dashboard'
 import LandingPage from './pages/LandingPage'
-import UserDashboard from './pages/UsersDashboard'
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-import EmailVerificationPage from './pages/EmailVerificationPage';
+import UserDashboard from './pages/UsersDashboard'  // Fixed: Changed from UsersDashboard to UserDashboard
+import EmailVerificationPage from './pages/EmailVerificationPage'
+import ResetPasswordPage from './pages/ResetPasswordPage'  // Added: Import for ResetPasswordPage
 
 function App() {
-
-	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/users/dashboard/" element={<UserDashboard />} />
-				<Route path="/auth" element={<AuthPage />} />
-				<Route path="/forgot-password" element={<ForgotPasswordPage />} />
-				<Route path="/reset-password" element={<ResetPasswordPage />} />
-				<Route path="/auth/verify-email" element={<EmailVerificationPage />} />
-			</Routes>
-		</Router>
-	)
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/users/dashboard/" element={<UserDashboard />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/forgot-password" element={<ResetPasswordPage />} />  {/* Fixed: Changed to ResetPasswordPage */}
+        <Route path="/auth/verify-email" element={<EmailVerificationPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />  {/* Added: Route for reset password */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
